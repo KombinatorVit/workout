418f63787664007a04a4f677de36f19f205a3d28
KombinatorVit <slonik0775@gmail.com>
2023-03-12 12:58:56 +0200

    add Header(Hamburger, Menu)

--- src/components/layout/header/Header.jsx
+++ src/components/layout/header/Header.jsx
@@ -1,7 +1,23 @@
+import { FiArrowLeft } from 'react-icons/fi'
+import { useAuth } from '../../../hooks/useAuth'
+import Hamburger from '../hamburger/Hamburger'
 import styles from './Header.module.scss'
 
-const Header = () => {
-    return <div className={styles.header}>Header</div>
+const Header = ({ backLink }) => {
+
+    const { isAuth } = useAuth()
+
+    return (
+        <header className={styles.header}>
+            <button onClick={() => {}}>
+                <FiArrowLeft />
+            </button>
+
+            fsdfg
+            {/* User profile */}
+            <Hamburger />
+        </header>
+    )
 }
 
 export default Header
