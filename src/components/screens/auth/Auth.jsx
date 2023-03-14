e34beae6fcff719c84d20a1caa7d0f9c12094a3f
KombinatorVit <slonik0775@gmail.com>
2023-03-14 15:18:45 +0200

    axios and request Token

--- src/components/screens/auth/Auth.jsx
+++ src/components/screens/auth/Auth.jsx
@@ -8,31 +8,43 @@ import Field from '../../ui/field/Field'
 import Layout from '../../layout/Layout'
 
 import styles from './Auth.module.scss'
+import {useMutation} from "@tanstack/react-query";
+import AuthService from "../../../services/auth.service.js";
 
-const isLoading = false
-const isLoadingAuth = false
 
 const Auth = () => {
-    const [type, setType] = useState('auth')
+    const [type, setType] = useState('login')
 
     const {
         register,
         handleSubmit,
-        formState: {errors}
+        formState: {errors},
+        reset
     } = useForm({
         mode: 'onChange'
     })
 
+
+    const {mutate, isLoading} = useMutation(
+        ['auth'],
+        ({email, password}) => AuthService.main(email, password, type),
+        {
+            onSuccess: data => {
+                alert('success')
+                reset()
+            }
+        }
+    )
     const onSubmit = data => {
         // type
-        console.log(data)
+        mutate(data.email, data.password)
     }
 
     return (
         <>
             <Layout heading='Sign in' bgImage='/images/auth-bg.png'/>
             <div className='wrapper-inner-page'>
-                {(isLoading || isLoadingAuth) && <Loader/>}
+                {(isLoading) && <Loader/>}
                 <form onSubmit={handleSubmit(onSubmit)}>
                     <Field
                         error={errors?.email?.message}
@@ -57,8 +69,8 @@ const Auth = () => {
                     />
 
                     <div className={styles.wrapperButtons}>
-                        <Button clickHandler={() => setType('auth')}>Sign in</Button>
-                        <Button clickHandler={() => setType('reg')}>Sign up</Button>
+                        <Button clickHandler={() => setType('login')}>Sign in</Button>
+                        <Button clickHandler={() => setType('register')}>Sign up</Button>
                     </div>
                 </form>
             </div>
--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,20 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './assets/styles/index.scss'
 import Routes from "./routes/Routes.jsx";
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query'
+
+
+const queryClient = new QueryClient({
+    defaultOptions: {
+        queries: {
+            refetchOnWindowFocus: false
+        }
+    }
+})
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <Routes/>
+    <QueryClientProvider client={queryClient}>
+
+        <Routes/>
+    </QueryClientProvider>
 )
